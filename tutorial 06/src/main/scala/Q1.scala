f69c8b17afedc54da631be7256fdce551773d788
Janod Umayanga <JanodUmayanga@gmail.com>
2022-09-05 22:05:00 +0530

    completed

--- tutorial 06/src/main/scala/Q1.scala
+++ tutorial 06/src/main/scala/Q1.scala
@@ -4,13 +4,13 @@ object Q1 extends App {
 
   val E = (c: Char, key: Int, a: String) => a((a.indexOf(c.toUpper) + key) % a.size)
 
-  val D = (c: Char, key: Int, a: String) => a((a.indexOf(c.toUpper) - key) % a.size)
+  val D = (c: Char, key: Int, a: String) => a((a.size + (a.indexOf(c.toUpper) - key)) % a.size)
 
   val cipher = (algo: (Char, Int, String) => Char, s: String, key: Int, a: String) => s.map(algo(_, key, a))
 
-  val key = 5
+  val key = 6
 
-  val ct = cipher(E, "uac", key, alphabet)
+  val ct = cipher(E, "HelloWorld", key, alphabet)
 
   val pt = cipher(D, ct, key, alphabet)
 
